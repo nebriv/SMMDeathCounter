6e4b3c040bbaf6e8b8b6bf37fef79aa196dc606b
nebriv <ben@benvirgilio.com>
2019-07-18 00:01:06 -0400

    Added ability to read twitch streams.

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # SMMDeathCounter
-Counts number of deaths for display while streaming Super Mario Maker!
+Counts number of deaths for display while streaming Super Mario Maker! Reads from a twitch stream!
 
 # Setup
 1. Make a python virtual environment.
 2. Run pip install -r requirements.txt
-3. Edit monitor.py line 10 to match the part of the screen you want to monitor. Format: (x,y,xy)
-3.a. Edit line 185 to your window widths make the script pause if your mouse leaves the game window area.
+3. Edit monitor.py line 16-20 (or at least set from_stream to false).
+    3.a. If you don't choose live stream edit game cords to match the part of the screen you want to monitor. Format: (x,y,xy)
+    3.b. Edit line 185 to your window widths make the script pause if your mouse leaves the game window area.
 4. ??? (Is this even a thing?)
 5. Profit (Run monitor.py)
 
-## Tips
+# Setup Tips
 1. Uncomment the while loop in lines 15-21 to wait for your mouse to entire the proper area. Leave the "if mouse_pos.x < 1920:" and "break" lines commented out if you just want the script to print out your current mouse position... helpful for determining the game window.
+
+# Known Issues
+1. Sometimes it double counts deaths. Yeah yeah I know. YOU HAD ONE JOB.
--- monitor.py
+++ monitor.py
@@ -6,9 +6,68 @@ import math
 import keyboard
 from directKeys import click, queryMousePosition, PressKey, ReleaseKey, SPACE, moveMouseTo
 from threading import Thread
+import webbrowser
+import livestreamer
+from livestreamer.plugins import twitch
+import livestreamer_cli
+from livestreamer import Livestreamer
 
 
+from_stream = True
+client_id = ""
+stream_url = ""
+# Doesn't need to be set unless you want to read from local screen
 game_coords = [283,235,1548,948]
+
+def authenticate_twitch_oauth():
+    # From livestream code - didn't feel like import it.
+    """Opens a web browser to allow the user to grant Livestreamer
+       access to their Twitch account."""
+
+    client_id = "***REMOVED***"
+    redirect_uri = "http://livestreamer.tanuki.se/en/develop/twitch_oauth.html"
+    url = ("https://api.twitch.tv/kraken/oauth2/authorize/"
+           "?response_type=token&client_id={0}&redirect_uri="
+           "{1}&scope=user_read+user_subscriptions").format(client_id, redirect_uri)
+
+    try:
+        if not webbrowser.open_new_tab(url):
+            raise webbrowser.Error
+    except webbrowser.Error:
+        print("Unable to open a web browser, try accessing this URL "
+                     "manually instead:\n{0}".format(url))
+
+if from_stream:
+
+    while client_id == "":
+        authenticate_twitch_oauth()
+        print("Opening browser, copy the client_id from the url and paste it in...")
+        client_id = input("Client ID: ")
+    while stream_url == "":
+        stream_url = input("Streamer URL: ")
+
+    session = Livestreamer()
+    session.set_option("http-headers", {"client-id":client_id})
+    session.set_option("hls-live-edge", 1)
+    #streams = session.streams("https://www.twitch.tv/xyz")
+    streams = session.streams(stream_url)
+
+    if len(streams) == 0:
+        print("No streams found on %s" % stream_url)
+        exit()
+
+    if "best" in streams:
+        stream = streams["best"]
+    else:
+        for each in streams.keys():
+            print(each)
+        stream = input("Choose a stream from above: ")
+        stream = streams[stream]
+
+    print("Capturing Best Stream...")
+    cap = cv2.VideoCapture(stream.url)
+elif game_coords:
+    pass
 
 
 #
@@ -145,7 +204,7 @@ def detect_death(frame):
     res2 = cv2.matchTemplate(gray_frame, template2, cv2.TM_CCOEFF_NORMED)
 
 
-    threshold = 0.8
+    threshold = 0.9
 
     loc = np.where(res >= threshold)
     loc2 = np.where(res2 >= threshold)
@@ -166,17 +225,75 @@ def detect_death(frame):
             detection = True
             cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
 
-    im_show(frame, "Mine Detection", 1, x=2500)
+    im_show(frame, "Death Detector", 1, x=2500)
     if detection:
         return True
     else:
         return False
+
 start = time.time()
 rips = 0
 thread = Thread(target=create_death_counter_image, args=())
 thread.setDaemon(True)
 thread.start()
 
+counter = 0
+restart = 3000
+
+
+# Read Stream
+while True:
+    succ, frame = cap.read()
+    if succ:
+        counter += 1
+        if counter < 60:
+            #im_show(frame, "Test", 1)
+            # Detect deaths
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            death = detect_death(frame)
+
+            if death:
+                start = time.time()
+                rips += 1
+                print("RIPs: %s" % rips)
+                black = False
+                print("waiting for black screen")
+                while not black:
+                    success, frame = cap.read()
+                    if success:
+                        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                        if np.any(frame[100, 100] == 0) and np.any(frame[50, 50] == 0) and np.any(frame[50, 150] == 0):
+                            black = True
+                    else:
+                        break
+
+
+                    # if color == 0:
+                    #     black = True
+
+                print("Waiting for game screen")
+                while black:
+                    success, frame = cap.read()
+                    if success:
+                        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                        if np.any(frame[100, 100] != 0) and np.any(frame[50, 50] != 0) and np.any(frame[50, 150] != 0):
+                            black = False
+                    else:
+                        break
+                print("Continuing")
+        else:
+            print("Catching up")
+            counter = 0
+            cap.read()
+
+
+    break
+cap.release()
+
+
+# Read image from screen
 while True:
 
     mouse_pos = queryMousePosition()
@@ -187,11 +304,31 @@ while True:
         # Capture Screen Grab
         img = ImageGrab.grab(bbox=game_coords)
         screen = np.array(img)
-
-        # Detect deaths
+        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         death = detect_death(screen)
+
         if death:
             start = time.time()
             rips += 1
             print("RIPs: %s" % rips)
-            time.sleep(3.5)
+            black = False
+            time.sleep(.1)
+            print("waiting for black screen")
+            while not black:
+                img = ImageGrab.grab(bbox=game_coords)
+                color = img.getpixel((50,50))
+                color2 = img.getpixel((100,100))
+                color3 = img.getpixel((150,150))
+                if color == (0,0,0) and color2 == (0,0,0) and color3 == (0,0,0):
+                    black = True
+
+            print("Waiting for game screen")
+            while black:
+                img = ImageGrab.grab(bbox=game_coords)
+                color = img.getpixel((50,50))
+                color2 = img.getpixel((100,100))
+                color3 = img.getpixel((150,150))
+                if color != (0,0,0) and color2 != (0,0,0) and color3 != (0,0,0):
+                    black = False
+            print("Continuing")
+            time.sleep(.2)
