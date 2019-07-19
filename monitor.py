c92e4091a9095a1c6cd8a285e49864a54c7534d1
nebriv <ben@benvirgilio.com>
2019-07-18 23:27:39 -0400

    Sped up detection by removing some inefficiencies.

--- monitor.py
+++ monitor.py
@@ -11,13 +11,15 @@ import livestreamer
 from livestreamer.plugins import twitch
 import livestreamer_cli
 from livestreamer import Livestreamer
-
+from queue import Queue
 
 from_stream = True
 client_id = ""
 stream_url = ""
 # Doesn't need to be set unless you want to read from local screen
 game_coords = [283,235,1548,948]
+
+background_color = ()
 
 def authenticate_twitch_oauth():
     # From livestream code - didn't feel like import it.
@@ -56,16 +58,18 @@ if from_stream:
         print("No streams found on %s" % stream_url)
         exit()
 
-    if "best" in streams:
-        print("Playing 'Best' Stream")
-        stream = streams["best"]
+    if "720p" in streams:
+        print("Playing '720p' Stream")
+        stream = streams["720p"]
+    elif "1080p" in streams:
+        print("Playing '1080p' Stream (WARNING THIS MIGHT BE LAGGY)")
+        stream = streams["1080p"]
     else:
         for each in streams.keys():
             print(each)
         stream = input("Choose a stream from above: ")
         stream = streams[stream]
 
-    print("Capturing Best Stream...")
     cap = cv2.VideoCapture(stream.url)
 elif game_coords:
     pass
@@ -124,70 +128,6 @@ def create_death_counter_image():
         #Display the image
         im_show(img, "Death Counter", 1, x=100)
 
-# def create_death_counter_image():
-#     global rips, start
-#     longest_time_since_death = None
-#     while True:
-#         time_since_last_death = round((time.time() - start),1)
-#         if longest_time_since_death:
-#             if time_since_last_death > longest_time_since_death:
-#                 longest_time_since_death = time_since_last_death
-#         else:
-#             longest_time_since_death = time_since_last_death
-#
-#         if rips > 0:
-#             longest_time_alive = longest_time_since_death
-#         else:
-#             longest_time_alive = "N/A"
-#
-#         # Create a black image
-#         img = np.zeros((60,380,3), np.uint8)
-#
-#         # Write some Text
-#         font                   = cv2.FONT_HERSHEY_SIMPLEX
-#         bottomLeftCornerOfText = (10,20)
-#         fontScale              = .8
-#         fontColor              = (255,255,255)
-#         lineType               = 2
-#
-#         cv2.putText(img,'Death Counter: %s' % rips,
-#             bottomLeftCornerOfText,
-#             font,
-#             fontScale,
-#             fontColor,
-#             lineType)
-#
-#         # Write some Text
-#         font                   = cv2.FONT_HERSHEY_SIMPLEX
-#         bottomLeftCornerOfText = (10,40)
-#         fontScale              = .5
-#         fontColor              = (255,255,255)
-#         lineType               = 1
-#
-#         cv2.putText(img,'Time since last death: %s seconds' % time_since_last_death,
-#             bottomLeftCornerOfText,
-#             font,
-#             fontScale,
-#             fontColor,
-#             lineType)
-#
-#         # Write some Text
-#         font                   = cv2.FONT_HERSHEY_SIMPLEX
-#         bottomLeftCornerOfText = (12,55)
-#         fontScale              = .5
-#         fontColor              = (255,255,255)
-#         lineType               = 1
-#
-#         cv2.putText(img,'   Longest time alive: %s seconds' % longest_time_alive,
-#             bottomLeftCornerOfText,
-#             font,
-#             fontScale,
-#             fontColor,
-#             lineType)
-#
-#         #Display the image
-#         im_show(img, "Death Counter", 1, x=100)
-
 template = cv2.imread("death.png", cv2.IMREAD_GRAYSCALE)
 w, h = template.shape[::-1]
 
@@ -196,39 +136,50 @@ w2, h2 = template.shape[::-1]
 
 def detect_death(frame):
     detection = False
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    res = cv2.matchTemplate(gray_frame, template, cv2.TM_CCOEFF_NORMED)
+
+    #gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #res = cv2.matchTemplate(gray_frame, template, cv2.TM_CCOEFF_NORMED)
     res2 = cv2.matchTemplate(gray_frame, template2, cv2.TM_CCOEFF_NORMED)
 
 
-    threshold = 0.9
+    threshold = 0.85
 
-    loc = np.where(res >= threshold)
+    #loc = np.where(res >= threshold)
     loc2 = np.where(res2 >= threshold)
-    for pt in zip(*loc[::-1]):
-        detection = True
-        cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
-
-    if len(loc[0]) > 0 and len(loc2[0] > 0):
-        for pt in zip(*loc[::-1]):
-            detection = True
-            cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
-    elif len(loc[0] > 0):
-        for pt in zip(*loc[::-1]):
-            detection = True
-            cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
-    elif len(loc2[0] > 0):
+    # for pt in zip(*loc[::-1]):
+    #     detection = True
+    #     cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
+    #
+    # if len(loc[0]) > 0 and len(loc2[0] > 0):
+    #     for pt in zip(*loc[::-1]):
+    #         detection = True
+    #         cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
+    # elif len(loc[0] > 0):
+    #     for pt in zip(*loc[::-1]):
+    #         detection = True
+    #         cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
+    if len(loc2[0] > 0):
         for pt in zip(*loc2[::-1]):
             detection = True
             cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
 
+    # else:
+    #     res = cv2.matchTemplate(gray_frame, template, cv2.TM_CCOEFF_NORMED)
+    #     loc = np.where(res >= threshold)
+    #     for pt in zip(*loc[::-1]):
+    #         detection = True
+    #         cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
     im_show(frame, "Death Detector", 1, x=2500)
     if detection:
         return True
     else:
         return False
+
+
+
+
+
 
 start = time.time()
 rips = 0
@@ -237,62 +188,62 @@ thread.setDaemon(True)
 thread.start()
 
 counter = 0
-restart = 3000
+restart = 90000
+
+print(cap.get(cv2.CAP_PROP_FPS))
+
+
 
 if from_stream:
     # Read Stream
     while True:
         succ, frame = cap.read()
+        #print(frame.shape)
+        #frame = cv2.resize(frame, (640, 480))
         if succ:
-            counter += 1
-            if counter < 60:
-                #im_show(frame, "Test", 1)
-                # Detect deaths
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                death = detect_death(frame)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            death = detect_death(frame)
 
-                if death:
-                    start = time.time()
-                    rips += 1
-                    print("RIPs: %s" % rips)
-                    black = False
-                    print("waiting for black screen")
-                    max = 1200
-                    counter2 = 0
-                    while not black:
-                        counter2 += 1
-                        if counter2 > max:
-                            break
-                        success, frame = cap.read()
-                        if success:
-                            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                            if np.any(frame[100, 100] == 0) and np.any(frame[50, 50] == 0) and np.any(frame[50, 150] == 0):
-                                black = True
-                        else:
-                            break
+            if death:
+                start = time.time()
+                rips += 1
+                print("RIPs: %s" % rips)
+                black = False
+                print("waiting for black screen")
+                max = 800
+                counter2 = 0
+                while not black:
+                    counter2 += 1
+                    if counter2 > max:
+                        print("Counter hit max, no black screen found")
+                        break
+                    success, frame = cap.read()
+                    if success:
+                        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                        if np.any(frame[100, 100] == 0) and np.any(frame[50, 50] == 0) and np.any(frame[50, 150] == 0):
+                            black = True
+                    else:
+                        break
 
-                        # if color == 0:
-                        #     black = True
+                    # if color == 0:
+                    #     black = True
 
-                    print("Waiting for game screen")
-                    counter2 = 0
-                    while black:
-                        counter2 += 1
-                        if counter2 > max:
-                            break
-                        success, frame = cap.read()
-                        if success:
-                            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                print("Waiting for game screen")
+                counter2 = 0
+                while black:
+                    counter2 += 1
+                    if counter2 > max:
+                        print("Counter hit max, no game screen found")
+                        break
+                    success, frame = cap.read()
+                    if success:
+                        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-                            if np.any(frame[100, 100] != 0) and np.any(frame[50, 50] != 0) and np.any(frame[50, 150] != 0):
-                                black = False
-                        else:
-                            break
-                    print("Continuing")
-            else:
-                print("Catching up")
-                counter = 0
-                cap.read()
+                        if np.any(frame[100, 100] != 0) and np.any(frame[50, 50] != 0) and np.any(frame[50, 150] != 0):
+                            black = False
+                    else:
+                        break
+                print("Continuing")
 
     cap.release()
 
